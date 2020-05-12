f559dc69e992fa960257777e978be3e7052cbea9
alexlezama90 <alexlezama90@hotmail.com>
2020-05-12 00:15:41 -0500

    End of Section 3

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -29,8 +30,13 @@ namespace API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddDbContext<StoreContext>(x => 
                 x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
+
+            //services.AddTransient This one only live for a single method <short lifetime>
+            //services.AddSingleton This lives as long as the application is online <long lifetime>           
+            services.AddScoped<IProductRepository, ProductRepository>(); //This one only lives for a request lifetime.
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
