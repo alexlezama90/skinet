38a4fd5b80f26ff7fd319b5c24f0e728e938ec46
alexlezama90 <alexlezama90@hotmail.com>
2020-05-12 15:46:02 -0500

    End of Section 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using API.Helpers;
+using AutoMapper;
 using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
@@ -37,6 +39,8 @@ namespace API
             //services.AddTransient This one only live for a single method <short lifetime>
             //services.AddSingleton This lives as long as the application is online <long lifetime>           
             services.AddScoped<IProductRepository, ProductRepository>(); //This one only lives for a request lifetime.
+            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
+            services.AddAutoMapper(typeof(MappingProfiles));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +54,8 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseStaticFiles();
 
             app.UseAuthorization();
 
